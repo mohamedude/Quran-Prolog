3de66a7405f3f5e4049ae99460b1ccc05b9f1163
Mohamed Benfasi <mohamed.benfasi@gmail.com>
2017-07-14 17:47:58 +0200

    First Commit

---
+++ quran-001.pl
@@ -0,0 +1,29 @@
+/*********************************************************************
+* I use quran.com as an english traduction, you can join us on slack
+* Al Fatiha
+**********************************************************************/
+
+/* In the name of Allah, the Entirely Merciful, the Especially Merciful. */
+merciful(allah).
+gracious(allah).
+
+/* [All] praise is [due] to Allah, Lord of the worlds - */
+praise(Y, X) :- praise(Y), god(X), god(X, _World).
+
+/* The Entirely Merciful, the Especially Merciful, */
+god(X) :- merciful(X), gracious(X).
+
+/* Sovereign of the Day of Recompense. */
+recompense(today).
+master(allah, recompense(today)).
+
+/* It is You we worship and You we ask for help. */
+worship(mohamed,allah).
+askforhelp(mohamed, allah).
+guide(allah, mohamed).
+
+/* Guide us to the straight path - */
+guide(mohamed, allah).
+
+/* The path of those upon whom You have bestowed favor, not of those who have evoked [Your] anger or of those who are astray. */
+guide(allah, Someone, straight(path)) :- bestowed(Someone), anger(allah, not(Someone)), astray(not(Someone)).
