a5511d334bff303132b4984655b2030fabf86d7b
Mohamed Benfasi <mohamed.benfasi@gmail.com>
2017-07-14 18:11:18 +0200

    First Commit

---
+++ quran-112.pl
@@ -0,0 +1,13 @@
+/*********************************************************************
+                            Al Ikhlas
+**********************************************************************/
+
+/* In the name of Allah, the Entirely Merciful, the Especially Merciful. */
+merciful(allah).
+gracious(allah).
+
+/* Say, "He is Allah, [who is] One, */
+one(allah).
+
+/* Allah, the Eternal Refuge. */
+absolute(allah).
